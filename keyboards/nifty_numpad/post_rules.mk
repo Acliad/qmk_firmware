9275c12d1c33d2d198528c475f322970cd845661
Isaac Rex <isaac.rex@garmin.com>
2024-11-13 08:13:23 -0600

    Added required rules.mk

--- keyboards/nifty_numpad/post_rules.mk
+++ keyboards/nifty_numpad/post_rules.mk
@@ -1,8 +1,3 @@
-<<<<<<<< HEAD:keyboards/nifty_numpad/keymaps/cadence/rules.mk
-TAP_DANCE_ENABLE=yes
-RGB_IDLE_ENABLE=yes
-========
 ifeq ($(strip $(RGB_IDLE_ENABLE)), yes)
     OPT_DEFS += -DRGB_IDLE_ENABLE
 endif
->>>>>>>> nifty-numpad-dev:keyboards/nifty_numpad/post_rules.mk
